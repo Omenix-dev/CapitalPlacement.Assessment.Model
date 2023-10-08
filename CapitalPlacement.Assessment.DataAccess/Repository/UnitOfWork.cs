a3d467adacf49e7716f70d1e63304a2f7b81befa
omenigph <omenigph@gmail.com>
2023-10-08 16:19:59 +0100

    added the dataAccess layer to the solution

---
+++ CapitalPlacement.Assessment.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using SpredMedia.Catalogue.Catalogue.Entity;
+using SpredMedia.Catalogue.Core.Interface;
+using System.IO;
+
+namespace SpredMedia.Catalogue.Infrastructure.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly CosmosClient _cosmosClient;
+        private IGenericRepository<Actor> _actorRepository;
+      
+        private IConfiguration _configuration;
+
+        public UnitOfWork(CosmosClient cosmosClient, IConfiguration config)
+        {
+            _cosmosClient = cosmosClient;
+            _configuration = config;
+        }
+
+        public IGenericRepository<Actor> ActorRepository => _actorRepository ??= new GenericRepository<Actor>(_database, "actors");
+    
+    }
+}
